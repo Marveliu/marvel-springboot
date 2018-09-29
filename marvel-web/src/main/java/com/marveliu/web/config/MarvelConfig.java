c6f325ecd57056b539e679199e18e8280491fb34
Marveliu <897920245@qq.com>
2018-09-29 17:10:35 +0800

    add:MarvelConfig

---
+++ marvel-web/src/main/java/com/marveliu/web/config/MarvelConfig.java
@@ -0,0 +1,10 @@
+package com.marveliu.web.config;
+
+/**
+ * @Author: Marveliu
+ * @Date: 2018/9/29 下午5:05
+ * @Description:
+ **/
+
+public class MarvelConfig {
+}
---
+++ marvel-web/src/main/java/com/marveliu/web/domain/vo/SysConfig.java
@@ -0,0 +1,10 @@
+package com.marveliu.web.domain.vo;
+
+/**
+ * @Author: Marveliu
+ * @Date: 2018/9/29 下午5:08
+ * @Description:
+ **/
+
+public class SysConfig {
+}
